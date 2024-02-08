cc914334bb719236624e4102917dd0bb625fe7dd
JoWo <pureth@googlemail.com>
2024-02-08 18:50:49 -0300

    remove comments

--- examples/drizzle/drizzle/0000_neat_christian_walker.sql
+++ examples/drizzle/drizzle/0000_neat_christian_walker.sql
@@ -9,6 +9,6 @@ CREATE TABLE `pubkey` (
 	`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	CONSTRAINT `pubkey_id` PRIMARY KEY(`id`)
 );
---> statement-breakpoint
-CREATE INDEX `pubkey__state__idx` ON `pubkey` (`state`);--> statement-breakpoint
+
+CREATE INDEX `pubkey__state__idx` ON `pubkey` (`state`);
 CREATE INDEX `pubkey__k1__idx` ON `pubkey` (`k1`);
